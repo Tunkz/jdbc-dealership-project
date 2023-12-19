a31f71810e95673082fc935eb8684f5ea6a150e9
Tunkz <Bubatunkz00@gmail.com>
2023-12-18 20:55:56 -0500

    Finished sales DAo and inventory Dao

--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -16,5 +16,21 @@ public class SalesDao {
 
     public void addSalesContract(SalesContract salesContract) {
         // TODO: Implement the logic to add a sales contract
+        String query = "INSERT INTO sales_contract VALUES (?, ?, ?, ?);";
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query)){
+
+            statement.setInt(1, salesContract.getContractId());
+            statement.setString(2, salesContract.getVin());
+            statement.setObject(3, salesContract.getSaleDate());
+            statement.setDouble(5, salesContract.getPrice());
+
+            int rows = statement.executeUpdate();
+            System.out.println("\nSales Contract added.");
+            System.out.println("Rows updated: " + rows);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
